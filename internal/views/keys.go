ee2053409cf1158b2b40f7dda233aaeb3e4b814d
onurhan <onurhanak@protonmail.com>
2025-05-04 12:24:55 -0400

    update keybindings

--- internal/views/keys.go
+++ internal/views/keys.go
@@ -3,7 +3,7 @@ package views
 var (
 	MainKeys            = "Search: </> | Switch Pane: <Tab> | Download Path: <^P> | API Key: <^X> | Quit: <^Q>"
 	TorrentsKeys        = "Up: <k> | Down: <j> | Add Magnet: <a> | Keybindings: <?>"
-	DetailsKeys         = "Up: <k> | Down: <j> | Copy link: <y> |  Download: <d> | Keybindings: <?>"
+	DetailsKeys         = "Up: <k> | Down: <j> | Copy link: <y> |  Download: <d> | Download all: <D> | Keybindings: <?>"
 	ActiveDownloadsKeys = "Up: <k> | Down: <j> | Status: <s> | Delete <d> | Keybindings: <?>"
 	SearchKeys          = "Search: <Enter> | Keybindings: <?>"
 	ModalKeys           = "Submit: <Enter> | Quit: <q>"
