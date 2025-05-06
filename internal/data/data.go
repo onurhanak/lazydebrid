d08c472b1b33c9b5c4b4839e1dacea8ee65144ac
onurhan <onurhanak@protonmail.com>
2025-05-06 13:31:42 -0400

    todo

--- internal/data/data.go
+++ internal/data/data.go
@@ -3,9 +3,10 @@ package data
 import "lazydebrid/internal/models"
 
 var (
-	UserDownloads    []models.Torrent
-	DownloadMap      = make(map[string]models.Torrent)
-	ActiveDownloads  []models.ActiveDownload
-	FilesMap         = make(map[string]models.Download)
-	TorrentLineIndex []string
+	UserDownloads     []models.Torrent
+	DownloadMap       = make(map[string]models.Torrent)
+	ActiveDownloads   []models.ActiveDownload
+	ActiveDownloadMap = make(map[string]models.ActiveDownload)
+	FilesMap          = make(map[string]models.Download)
+	TorrentLineIndex  []string
 )
